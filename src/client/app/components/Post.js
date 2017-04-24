47e96255b945c3b8b580369d3e5470f64fe36eac
kazixma <kazixma@gmail.com>
2017-04-24 22:12:45 +0700

    add post components

---
+++ src/client/app/components/Post.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Post extends React.Component {
+  render() {
+    return (
+      <div className="ui card">
+        <div className="content">
+          <div className="right floated meta">14h</div>
+        </div>
+        <div className="image">
+
+        </div>
+        <div className="content">
+          <span className="right floated">
+            <i className="heart outline like icon"></i>
+            17 likes
+           </span>
+          <i className="comment icon"></i>
+          3 comments
+       </div>
+        <div className="extra content">
+          <div className="ui large transparent left icon input">
+            <i className="heart outline icon"></i>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Post;
--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,35 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Post from './components/Post.js';
 
-
-class Hello extends React.Component {
-  render() {
-    return (
-      <div className="ui card">
-        <div className="content">
-          <div className="right floated meta">14h</div>
-        </div>
-        <div className="image">
-
-        </div>
-        <div className="content">
-          <span className="right floated">
-            <i className="heart outline like icon"></i>
-            17 likes
-           </span>
-          <i className="comment icon"></i>
-          3 comments
-       </div>
-        <div className="extra content">
-          <div className="ui large transparent left icon input">
-            <i className="heart outline icon"></i>
-
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Post/>, document.getElementById('root'));
 
